cd07aa5fd68217ec8e88a2cb3245e7631069fa95
shital <shitalbabar08@gmail.com>
2023-03-08 13:27:15 +0530

    second Update

---
+++ src/io/shital/RestApiTest.java
@@ -0,0 +1,13 @@
+package io.shital;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	@Test
+	public void  faceBookAdd() {
+		System.out.println("Add 1");
+		System.out.println("Add 2");
+		
+	}
+
+}
